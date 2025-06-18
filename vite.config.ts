ef54843d2dca3a99280a2eb7bed3bf851e2260e4
Alex Rocha <alexrocha.f@outlook.com>
2025-06-17 23:40:04 -0400

    build: comment out GitHub Pages base path in vite config
    The base path is no longer needed as we're deploying to a different environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/deathspace-coruja/', // caminho correto para GitHub Pages
+  //base: '/deathspace-coruja/', // caminho correto para GitHub Pages
   plugins: [react()],
 })
